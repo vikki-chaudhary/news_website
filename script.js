bb66d910c88d1eff35b2bd3857f24ea631867f72
vikki-chaudhary <150070558+vikki-chaudhary@users.noreply.github.com>
2023-12-02 03:30:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ function reload() {
 async function fetchNews(query) {
     const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
     const data = await res.json();
+    console.log(123);
+    console.log(data);
     bindData(data.articles);
 }
 
